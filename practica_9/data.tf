407f68b500eed7d744d467d8968ccefc23e37922
AgusTinux <agarcia71@gmail.com>
2024-10-01 21:24:46 -0500

    Cambios en Practica 9

---
+++ practica_9/data.tf
@@ -0,0 +1,3 @@
+data "aws_key_pair" "myKey" {
+  key_name = "bnkcomm-access-key"
+}
--- practica_9/remote-exec-provisioner.tf
+++ practica_9/remote-exec-provisioner.tf
@@ -1,25 +1,21 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_instance" "web_server" {
-  ami           = "ami-2757f631" # Reemplaza con la AMI que desees
+  ami           = "ami-0230bd60aa48260c6"
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.my_key.key_name
+  key_name      = data.aws_key_pair.myKey.key_name
 
   provisioner "remote-exec" {
     connection {
       type        = "ssh"
-      user        = "ec2-user"                 # Usuario predeterminado para Amazon Linux
-      private_key = file(var.private_key_path) # Llave privada para conectarse a la instancia
+      user        = "ec2-user"                       # Usuario predeterminado para Amazon Linux
+      private_key = data.aws_key_pair.myKey.key_name # Llave privada para conectarse a la instancia
       host        = self.public_ip
     }
 
     inline = [
-      "sudo yum update -y",
-      "sudo yum install -y httpd",   # Instala Apache
-      "sudo systemctl start httpd",  # Inicia el servicio Apache
-      "sudo systemctl enable httpd", # Habilita Apache para que inicie automáticamente
+      "apt update -y",
+      "apt install -y apache2",   # Instala Apache
+      "systemctl start apache2",  # Inicia el servicio Apache
+      "systemctl enable apache2", # Habilita Apache para que inicie automáticamente
       "echo '<h1>Bienvenido al servidor web</h1>' | sudo tee /var/www/html/index.html"
     ]
   }
@@ -27,11 +23,6 @@ resource "aws_instance" "web_server" {
   tags = {
     Name = "Terraform-Web-Server"
   }
-}
-
-resource "aws_key_pair" "my_key" {
-  key_name   = "my-key"
-  public_key = file(var.public_key_path) # Llave pública que usarás para conectarte a la instancia
 }
 
 resource "aws_security_group" "web_sg" {
